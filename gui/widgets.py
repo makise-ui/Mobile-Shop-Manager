ad2591b2b9ee53c74db402794fbe3a64b4e7a7dc
makise-ui <makiseui@gmail.com>
2026-01-15 09:39:21 +0530

    feat(ui): Add CollapsibleFrame widget

--- gui/widgets.py
+++ gui/widgets.py
@@ -16,3 +16,37 @@ class IconButton(tb.Button):
     def update_icon(self, new_image):
         self.configure(image=new_image)
         self.image = new_image
+
+class CollapsibleFrame(ttk.Frame):
+    """
+    A Frame designed to be toggled visibility.
+    Assumes it is managed via pack().
+    """
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self._is_expanded = False
+        self._pack_opts = {}
+        
+    def show(self, **pack_opts):
+        """Show the frame using pack geometry manager."""
+        self._pack_opts = pack_opts
+        self.pack(**pack_opts)
+        self._is_expanded = True
+        
+    def hide(self):
+        """Hide the frame."""
+        self.pack_forget()
+        self._is_expanded = False
+        
+    def toggle(self, **pack_opts):
+        """Toggle visibility."""
+        if self._is_expanded:
+            self.hide()
+        else:
+            # Use stored pack options if none provided
+            opts = pack_opts if pack_opts else self._pack_opts
+            self.show(**opts)
+            
+    @property
+    def is_expanded(self):
+        return self._is_expanded
